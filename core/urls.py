910c70f0bbbe1006f195d8f6c8b44aa8570ef92d
sistemas <sistemas@SPO_LAB51301.inacap.cl>
2025-12-04 16:56:54 -0300

    Agregar página de inicio simple

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.http import HttpResponse
+
+def simple_home(request):
+    return HttpResponse("""
+    <h1>API de Productos</h1>
+    <p>API REST con Django DRF desplegada en Render</p>
+    <ul>
+        <li><a href="/api/productos/">/api/productos/</a> - API Productos</li>
+        <li><a href="/admin/">/admin/</a> - Panel Admin</li>
+    </ul>
+    """)
 
 urlpatterns = [
+    path('', simple_home, name='home'),
     path('admin/', admin.site.urls),
     path('', include('productos.urls')),
 ]
